d049c0edc128cde18b6dacc9652a6e2e61a1352d
Thanawit Suwannikom <thanawit@Thanawits-MacBook-Pro-2.local>
2024-06-14 00:41:16 +0700

    add save img

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def upload_image():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        # file.save(filepath)
+        file.save(filepath)
         
         # Process the image and print EXIF data
         exif_data = process_image(filepath)
